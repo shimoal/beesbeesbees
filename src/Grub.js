13294539cc0d9b9769786d68e66984c6d7549919
Alison Reed <alison.michelle.reed@gmail.com>
2016-12-19 11:07:46 -0800

    Pass tests for Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,18 @@
 var Grub = function() {
+	this.age = 0;
+	this.color = 'pink';
+	this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {
+	return;
+};
+
+var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+};
+
+// Bee.prototype = Object.create(Grub.prototype);
+
+// Bee.prototype.constructor = Bee;
